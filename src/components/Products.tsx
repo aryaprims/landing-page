1383f7e83442eb8bb0670addadbbffbf72fe3dd3
aryaprims <aryaprima07@gmail.com>
2024-08-26 19:10:20 +0700

    figma add

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -4,7 +4,7 @@ export default function Products() {
       <div className='container flex flex-col gap-6'>
         <h2 className='text-4xl text-center font-semibold'>Products</h2>
         <div className='lg:w-3/4 lg:mx-auto flex justify-center'>
-          <img src='/web-illustrator-3.png' className='lg:w-3/4' alt='' />
+          <img src='/figma1.png' className='lg:w-3/4' alt='' />
         </div>
       </div>
     </section>
