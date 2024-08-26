21ae037a033581b092559a2b9223eb5b755d43fb
aryaprims <aryaprima07@gmail.com>
2024-08-26 20:06:48 +0700

    Center button

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,7 +1,7 @@
 export default function Products() {
   return (
     <section id='product' className='py-32'>
-      <div className='container flex flex-col gap-6'>
+      <div className='container flex flex-col gap-6 items-center'>
         <h2 className='text-4xl text-center font-semibold'>Products</h2>
         <div className='lg:w-3/4 lg:mx-auto flex justify-center'>
           <img src='/figma1.png' className='lg:w-3/4' alt='' />
