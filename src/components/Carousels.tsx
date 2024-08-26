503f16378ec7528c5acbd41f5b0cb754c608da70
aryaprims <aryaprima07@gmail.com>
2024-08-27 02:05:53 +0700

    carousel editss

--- src/components/Carousels.tsx
+++ src/components/Carousels.tsx
@@ -5,7 +5,7 @@ export default function Carousels() {
   return (
     <section id='carousel' className='py-32 bg-slate-100 mt-20'>
       <div className='container'>
-        <h2 className='text-4xl text-center font-semibold mb-10'>Our Moments</h2> {/* Menambahkan margin bawah */}
+        <h2 className='text-4xl text-center font-semibold mb-10'>Our Moments</h2>
         <div className='lg:w-3/4 lg:mx-auto'>
           <Carousel 
             showThumbs={false} 
@@ -15,15 +15,27 @@ export default function Carousels() {
             showStatus={false}
           >
             <div>
-              <img src='/momen4.jpg' alt='Slide 1' className='rounded-xl mx-auto max-h-64'/> {/* Mengatur ukuran gambar */}
+              <img 
+                src='/momen4.jpg' 
+                alt='Slide 1' 
+                className='rounded-xl mx-auto object-cover w-64 h-64' 
+              />
               <p className='legend'>2nd meet as Niagara 4</p>
             </div>
             <div>
-              <img src='/momen2.jpg' alt='Slide 2' className='rounded-xl mx-auto max-h-64'/>
+              <img 
+                src='/momen2.jpg' 
+                alt='Slide 2' 
+                className='rounded-xl mx-auto object-cover w-64 h-64' 
+              />
               <p className='legend'>Caption for Slide 2</p>
             </div>
             <div>
-              <img src='/momen3.jpg' alt='Slide 3' className='rounded-xl mx-auto max-h-64'/>
+              <img 
+                src='/momen3.jpg' 
+                alt='Slide 3' 
+                className='rounded-xl mx-auto object-cover w-64 h-64' 
+              />
               <p className='legend'>First Meet as Niagara 4</p>
             </div>
           </Carousel>
