a2d275266ba461a835f5f90647747e6ce03e65f2
aryaprims <aryaprima07@gmail.com>
2024-08-26 19:35:20 +0700

    figma link

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,6 +6,12 @@ export default function Products() {
         <div className='lg:w-3/4 lg:mx-auto flex justify-center'>
           <img src='/figma1.png' className='lg:w-3/4' alt='' />
         </div>
+        <a
+              href='https://www.figma.com/proto/NAFZFZQFovC7AY4T5ZZS02/GROUP4_PIJAR-SEKOLAH?node-id=25-17196&t=ERMxsOi7VjS5s0Wg-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=25%3A17196'
+              className='py-4 px-8 bg-teal-500 text-white rounded-xl w-fit'
+        >
+              Test Prototype
+        </a>
       </div>
     </section>
   )
